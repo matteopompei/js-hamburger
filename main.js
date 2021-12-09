68041aaad8d4feca3b6cb7c2b732acaf9023e88c
Matteo Pompei <datxizzan@gmail.com>
2021-12-09 19:35:28 +0100

    Commenti extra

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
-const barre = document.querySelector(".fa-bars");
-const crocetta = document.querySelector(".fa-times");
-const hamburgerMenu = document.querySelector(".hamburger-menu");
+const barre = document.querySelector(".fa-bars"); // Qui selezioni l'icona del menù
+const crocetta = document.querySelector(".fa-times"); // Qui selezioni la crocetta del menù (per chiuderlo)
+const hamburgerMenu = document.querySelector(".hamburger-menu"); // Qui selezioni il menu hamburger
 
-barre.onclick = function() {menuVisibile()};
-crocetta.onclick = function() {menuNascosto()};
+barre.onclick = function() {menuVisibile()}; // Qui parte la funzione menuVisibile quando clicchi su sull'icona del menù
+crocetta.onclick = function() {menuNascosto()}; // Qui parte la funzione menuNascosto quando clicchi sulla crocetta (per chiuderlo)
 
+// Questa è la funzione che dice di aggiungere la classe .active ad .hamburger-menu
 function menuVisibile() {
   hamburgerMenu.classList.add("active");
 }
 
+// Questa è la funzione che dice di rimuovere la classe .active ad .hamburger-menu
 function menuNascosto() {
   hamburgerMenu.classList.remove("active");
 }
