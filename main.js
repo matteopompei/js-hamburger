877a9c1d8552cac51e9d5dba22d3505ca1343f0e
Matteo Pompei <datxizzan@gmail.com>
2021-12-09 16:36:56 +0100

    Hamburger menù completato

--- main.js
+++ main.js
@@ -1,9 +1,14 @@
 const barre = document.querySelector(".fa-bars");
+const crocetta = document.querySelector(".fa-times");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 
 barre.onclick = function() {menuVisibile()};
+crocetta.onclick = function() {menuNascosto()};
 
 function menuVisibile() {
   hamburgerMenu.classList.add("active");
 }
 
+function menuNascosto() {
+  hamburgerMenu.classList.remove("active");
+}
